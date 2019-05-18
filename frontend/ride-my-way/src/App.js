9c0d60573f842305e1af247471b1665cdc3670f4
mwinamijr <mwinamijr@gmail.com>
2019-05-18 15:27:17 +0300

    modified files

--- frontend/ride-my-way/src/App.js
+++ frontend/ride-my-way/src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import offer from './images/offers.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+     return (
+        <div>
+           <header>
+            <nav className="">
+                <ul>
+                    <li><a className="active" href="#">Home</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a class="float-right" href="profile.html">Profile</a></li>
+                    <li><a class="float-right" href="signin.html">Login</a></li>
+                  </ul>
+            </nav>
+          </header>
+
+          <main>
+            <div className="container">
+              <div className="container">
+                <h1 className="myhead">Ride My Ride</h1>
+              </div>
+              <h2 className="head">Ride offers</h2>
+              <img src={offer} alt="offers" />
+            </div>
+
+            <div className="container">
+                <div className="custom-select">
+                    <h2>Offers selection</h2>
+                    <select>
+                        <option value="0">Select offer:</option>
+                        <option value="1">One Way</option>
+                        <option value="2">Return Trip</option>
+                        <option value="3">Family Package</option>
+                        <option value="4">Custom Cars</option>
+                    </select>
+                </div>
+            </div>
+            <div className="container" id="account">
+                <p>Do you have an account? <a href="signin.html">Login</a> or <a href="signup.html">Sign Up</a></p>
+            </div>
+          </main>
+          <footer>
+            <p>&copy; Copyright 2019</p>
+          </footer>
+          <script src="main.js"></script>
+        </div>
+     );
+  }
 }
 
 export default App;
