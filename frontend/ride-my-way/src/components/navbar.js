23edf0e0e7d1dfee945733400c1dc52c36a4c50e
mwinamijr <mwinamijr@gmail.com>
2019-05-21 18:41:28 +0300

    modified navbar.js

--- frontend/ride-my-way/src/components/navbar.js
+++ frontend/ride-my-way/src/components/navbar.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import './index.css';
 
-const toolbar = props => {
-  <header>
-    <nav>
-      <ul>
-        <li><a className="active" href="#">Home</a></li>
-        <li><a href="contact.html">Contact</a></li>
-        <li><a class="float-right" href="profile.html">Profile</a></li>
-        <li><a class="float-right" href="signin.html">Login</a></li>
-      </ul>
-    </nav>
-  </header>
-};
-
-
 function navbar() {
   return (
     <header>
