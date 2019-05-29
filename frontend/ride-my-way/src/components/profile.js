b0285333c7ee2cadb198d85c2d066791ee0583bd
mwinamijr <mwinamijr@gmail.com>
2019-05-29 05:30:27 +0300

    added contents of profile file

---
+++ frontend/ride-my-way/src/components/profile.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar"
+import Footer from './footer'
+import Avatar from '../assets/images/default.png'
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div>
+            <div className="p-container">
+                <div className="avatar">
+                  <img src={Avatar} height="150" width="150" />
+                </div>
+                <h2>Mwinami JR</h2>
+                <h4>PRO DRIVER</h4>
+                <p>Am a good driver with a 4 years of driving experience</p>
+            </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Profile;
